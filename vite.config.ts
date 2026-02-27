d45a058d4e71abcc04a87f4b550dbbd46466597b
william <cheny0976@gmail.com>
2026-02-27 22:12:58 +0800

    add dist for deploy

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// NOTE: base 設為 './' 以支援 GitHub Pages 部署
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-});
